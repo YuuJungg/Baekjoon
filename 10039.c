4935d1cf20b4527df7e6ed9a644f5dfc14dbf34f
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-05-25 20:23:01 +0900

    Add files via upload
    코드 수정

--- 10039.c
+++ 10039.c
@@ -2,24 +2,20 @@
 
 int main()
 {
-	int one, two, three, four, five;
-
-	scanf("%d %d %d %d %d", &one, &two, &three, &four, &five);
-
-	if (one < 40)
-		one = 40;
-	if (two < 40)
-		two = 40;
-	if (three < 40)
-		three = 40;
-	if (four < 40)
-		four = 40;
-	if (five < 40)
-		five = 40;
-
-	int result = (one + two + three + four + five) / 5;
-	printf("%d\n", result);
-
-
+	int temp = 0;
+	int result = 0;
+	
+	for(int i=0; i<5; i++)
+	{
+		scanf("%d", &temp);
+		if(temp < 40)
+			temp = 40;	
+		result += temp;
+	}
+	
+	printf("%d\n", result/5);
+	
+		
+	
 
 }
